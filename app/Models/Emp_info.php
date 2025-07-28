1211a38c90a8246f89a67fdfaebb0851b72081b9
EU <riodelacruz033@gmail.com>
2025-07-29 07:47:32 +0800

    add connection to the employment and department

--- app/Models/Emp_info.php
+++ app/Models/Emp_info.php
@@ -12,4 +12,13 @@ class Emp_info extends Model
     protected $fillable = ['first_name', 'last_name', 'middle_name','birth_date','gender',
         'marital_status', 'contact_number', 'address',
         ];
+    public function employment()
+    {
+        return $this->hasOne(Employment_info::class, 'employee_id', 'emp_id');
+    }
+
+    public function department()
+    {
+        return $this->hasOne(Department::class, 'employee_id', 'emp_id');
+    }
 }
