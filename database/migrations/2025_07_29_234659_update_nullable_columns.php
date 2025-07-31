a6dce32b084471526bfea639d1ccd6613223836a
EU <riodelacruz033@gmail.com>
2025-07-31 08:33:40 +0800

    migration

--- database/migrations/2025_07_29_234659_update_nullable_columns.php
+++ database/migrations/2025_07_29_234659_update_nullable_columns.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::table('employment_info', function (Blueprint $table) {
             $table->date('regularization')->nullable()->change();
             $table->date('contract_end')->nullable()->change();
+            $table->string('companyEmail')->nullable()->change();
+            $table->string('alternativeEmail')->nullable()->change();
         });
         Schema::table('emp_info', function (Blueprint $table) {
             $table->string('middle_name')->nullable()->change();
@@ -29,6 +31,8 @@ return new class extends Migration
         Schema::table('employment_info', function (Blueprint $table) {
             $table->date('regularization')->nullable(false)->change();
             $table->date('contract_end')->nullable(false)->change();
+            $table->string('companyEmail')->nullable(false)->change();
+            $table->string('alternativeEmail')->nullable(false)->change();
 
         });
         Schema::table('emp_info', function (Blueprint $table) {
