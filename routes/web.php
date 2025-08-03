309532c230f10a5c1b3d378f665ae17af902032b
EU <riodelacruz033@gmail.com>
2025-08-03 09:05:13 +0800

    apply middlware and added new route for update employee

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\EmployeeInfoController;
 use App\Http\Controllers\UserController;
+use App\Http\Middleware\checkUser;
+use App\Http\Middleware\HRSupervisorUser;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -16,30 +18,30 @@ Route::get('/', function () {
 //employee pages
 Route::get('/Employee/{type}', function ($type) {
     return view('pages.employee_table', ['type' => $type]);
-});
+})->middleware('check.user');
 
 Route::get('Forms/AddEmployee', function () {
     return view('pages.addmep');
-});
+})->middleware('check.user');
 Route::get('Forms/EditEmployee', function () {
     return view('pages.editemp');
-});
+})->middleware('check.user');
 //employee data
-Route::get('/Employee/Table/{type}', [EmployeeInfoController::class, 'getEmployees']);
+Route::get('/Employee/Table/{type}', [EmployeeInfoController::class, 'getEmployees'])->middleware('check.user');
 
 
 //EmployeeInfo post
 
-Route::post('/employee/new-employee', [EmployeeInfoController::class, 'addEmployee']);
+Route::post('/employee/new-employee', [EmployeeInfoController::class, 'addEmployee'])->middleware('check.user');
 
 
 Route::get('/Users', function () {
     return view('pages.users');
-});
+})->middleware('check.user');
 
 //api
-Route::get('/api/employees/{type}', [EmployeeInfoController::class, 'getEmployees']);
-Route::get('api/employee',[EmployeeInfoController::class, 'getEmployee']);
+Route::get('/api/employees/{type}', [EmployeeInfoController::class, 'getEmployees'])->middleware('check.user');
+Route::get('api/employee',[EmployeeInfoController::class, 'getEmployee'])->middleware('check.user');
 
 
 
@@ -59,3 +61,5 @@ Route::post('/userlogin', [UserController::class, 'login']);
 Route::post('/userregister', [UserController::class, 'register']);
 
 
+//update
+Route::put('/employee/update-employee' , [EmployeeInfoController::class, 'updateEmployee'])->middleware('check.user');
